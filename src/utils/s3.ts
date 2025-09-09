e9dfe57b422ffbc74e5d7a9a5c3b2832346f76d7
harryli0088 <harryli0088@gmail.com>
2025-09-09 14:39:46 -0400

    missed changes

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,4 +1,7 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+if(!process.env.S3_BUCKET_NAME) throw new Error("You mus define the S3_BUCKET_NAME environment variable")
+export const BUCKET_NAME = process.env.S3_BUCKET_NAME
 
 // Initialize S3 client
 export const s3 = new S3Client({
@@ -11,9 +14,9 @@ export const s3 = new S3Client({
   forcePathStyle: true, // Required for some S3-compatible providers
 });
 
-export async function getS3File({Bucket,filename,s3}:{Bucket:string,filename:string,s3:S3Client}) {
+export async function getS3File({Bucket,filename,s3Client}:{Bucket:string,filename:string,s3Client:S3Client}) {
   try {
-    const s3Response = await s3.send(
+    const s3Response = await s3Client.send(
       new GetObjectCommand({
         Bucket,
         Key: filename,
@@ -29,4 +32,16 @@ export async function getS3File({Bucket,filename,s3}:{Bucket:string,filename:str
     console.error(err)
     throw new Error("Failed to retrieve file")
   }
+}
+
+type UploadS3ObjectArgsType = {
+  Bucket: string;
+  Key: string;
+  Body: Buffer<ArrayBufferLike>;
+  ContentType: string;
+  ContentLength: number;
+}
+
+export async function uploadS3Object(s3Client:S3Client, args:UploadS3ObjectArgsType) {
+  return await s3Client.send( new PutObjectCommand(args) );
 }
